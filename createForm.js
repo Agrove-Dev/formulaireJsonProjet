5947bbc0aa5f60f6e4c2fbc9cad85f4ff4f63b81
Agrove <74765821+Agrove-Dev@users.noreply.github.com>
2021-09-22 17:35:23 +0200

    Formulaire beta

---
+++ createForm.js
@@ -0,0 +1,16 @@
+function download(filename, data) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + JSON.stringify(data));
+    element.setAttribute('download', filename+".json");
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+    document.body.inden
+  }
+  
+  // Start file download.
+  download(filename+".json","File content");
--- form.js
+++ form.js
@@ -1,19 +1,16 @@
-// import * as Fs from 'fs'
 
-// export function createForm() {
-// const rq = require(Fs);
-// rq
 let namePlant = document.getElementById("exampleFormControlInput1");
 let descriptionPlant = document.getElementById("exampleFormControlInput2");
 let categorieSelected = document.querySelectorAll("selectCategorie");
+let characteristicHeight = document.getElementById("characteristicHeight")
 
 var association = new Object();
-association["ail"] = true;
-association["begonia"] = true;
-association["capucine"] = true;
-association["cassis"] = true;
-association["cerfeuil"] = true;
-association["cosmos"] = true;
+// association["ail"] = true;
+// association["begonia"] = true;
+// association["capucine"] = true;
+// association["cassis"] = true;
+// association["cerfeuil"] = true;
+// association["cosmos"] = true;
 
 var characteristic = new Object();
 characteristic["exhibition"] = -1;
@@ -25,26 +22,20 @@ characteristic["water"] = -1;
 
 var filtre = new Object();
 
-var exposition
+var characteristic = new Object();
 
 var flowingPeriod = new Object();
-flowingPeriod["endMonth"] = -1;
-flowingPeriod["startMonth"] = -1;
 
 var harvestPeriod = new Object();
-harvestPeriod["endMonth"] = -1;
-harvestPeriod["startMonth"] = -1;
 
 var plantingPeriod = new Object();
-plantingPeriod["endMonth"] = -1;
-plantingPeriod["startMonth"] = -1;
 
 var sowingPeriod = new Object();
-sowingPeriod["endMonth"] = -1;
-sowingPeriod["startMonth"] = -1;
+
 
 function initForm() {
 
+    //GET ALL VALUE OF FILTRE
     for (var option of document.getElementById('selectCategorie').options)
     {
         if (option.selected) {
@@ -52,10 +43,59 @@ function initForm() {
         }
     }
 
+    //GET ALL VALUE OF EXPOSITION
+    for (var option of document.getElementById('characteristicExposition').options)
+    {
+        if (option.selected) {
+            characteristic["exhibition"] = parseInt(option.value);
+        }
+    }
+
+    //GET ALL VALUE OF WATER NEEDED
+    for (var option of document.getElementById('characteristicWater').options)
+    {
+        if (option.selected) {
+            characteristic["water"] = parseInt(option.value);
+        }
+    }
+
+        //GET ALL VALUE OF RUSTICITE
+    for (var option of document.getElementById('characteristicRusticite').options)
+    {
+        if (option.selected) {
+             characteristic["rusticite"] = parseInt(option.value);
+        }
+    }
+
+    //GET ALL VALUE OF USDA
+    for (var option of document.getElementById('characteristicUsda').options)
+    {
+        if (option.selected) {
+            characteristic["usda"] = option.value;
+        }
+    }
+
+    //GET ALL VALUE OF PH
+    for (var option of document.getElementById('characteristicPh').options)
+    {
+        if (option.selected) {
+            characteristic["ph"] = parseInt(option.value);
+        }
+    }
+
+    characteristic["height"] = characteristicHeight.value;
+    sowingPeriod["startMonth"] = parseInt(document.getElementById("semisStart").value);
+    sowingPeriod["endMonth"] = parseInt(document.getElementById("semisEnd").value);
+    harvestPeriod["startMonth"] = parseInt(document.getElementById("recolteStart").value);
+    harvestPeriod["endMonth"] = parseInt(document.getElementById("recolteEnd").value);
+    flowingPeriod["startMonth"] = parseInt(document.getElementById("floraisonStart").value);
+    flowingPeriod["endMonth"] = parseInt(document.getElementById("floraisonEnd").value);
+    plantingPeriod["startMonth"] = parseInt(document.getElementById("plantationStart").value);
+    plantingPeriod["endMonth"] = parseInt(document.getElementById("plantationEnd").value);
+
     let data = {
         "name" : namePlant.value,
         "description" : descriptionPlant.value,
-        association,
         characteristic,
         filtre,
         flowingPeriod,
@@ -63,18 +103,7 @@ function initForm() {
         plantingPeriod,
         sowingPeriod
     };
+
+    download("plant", data);
     console.log(data);
 }
-
-    // // On transforme le JSON en chaine de caractères
-    // let dataStringified = JSON.stringify(data, null, 2);
-    
-    // fs.writeFile('data.json', dataStringified, (err) => {
-    //     if (err) throw err;
-    //     console.log('JSON écrit !');
-    // });
-    
-    // console.log('Après le lancement de l\'écriture');
-    
-
-// }
