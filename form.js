f241db1ffb0a7c50ca29f5e9df70896cc8ef49bd
Agrove <74765821+Agrove-Dev@users.noreply.github.com>
2021-09-29 15:39:01 +0200

    top

--- form.js
+++ form.js
@@ -1,5 +1,6 @@
 //DOM INTERFACE
-
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
 function initDom() {
     let domDecoded = {};
